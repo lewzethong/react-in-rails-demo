80c91a733d38992611abe12a022c4e9816addaa2
lewzethong <lewzethong@gmail.com>
2022-11-06 17:42:05 +0800

    all functionality working

---
+++ app/javascript/src/home.jsx
@@ -0,0 +1,18 @@
+// home.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Layout from './layout';
+import './home.scss';
+
+const Home = () => (
+  <Layout>
+    <h1>Home page</h1>
+  </Layout>
+)
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Home />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
